549c85a7e7caeeb7d51ce5cd35aaeb56984fed4e
Nina Nikitenko <ninanienliya@gmail.com>
2024-08-16 01:22:09 +0300

    Реализован CRUD для пользователей, настроено использование JWT-авторизации и каждый эндпоинт закрыт авторизацией.
    Эндпоинты для авторизации и регистрации доступны для неавторизованных пользователей.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
-from users.views import PaymentListApiView
+from users.views import PaymentListApiView, UserCreateApiView, UserListApiView, UserRetrieveApiView, UserUpdateApiView, \
+    UserDestroyApiView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path("", UserListApiView.as_view(), name="users_list"),
+    path("<int:pk>/", UserRetrieveApiView.as_view(), name="user_retrieve"),
+    path("<int:pk>/update/", UserUpdateApiView.as_view(), name="user_update"),
+    path("<int:pk>/delete/", UserDestroyApiView.as_view(), name="user_destroy"),
     path("payments/", PaymentListApiView.as_view(), name="payments_list"),
+    path('register/', UserCreateApiView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(permission_classes = [AllowAny]), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes = [AllowAny]), name='token_refresh'),
 ]
