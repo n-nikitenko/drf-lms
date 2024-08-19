61883fb0db45a820a757f6361503d3672be3f27e
nnikitenko <nnikitenko@aorti.ru>
2024-08-19 15:09:42 +0300

    Описаны права доступа для объектов таким образом, чтобы пользователи, которые не входят в группу модераторов, могли видеть, редактировать и удалять только свои курсы и уроки.

--- materials/views.py
+++ materials/views.py
@@ -1,11 +1,13 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
+from materials.permissions import IsModeratorPermission, IsOwnerPermission
 from materials.serializers import CourseSerializer, LessonSerializer
-from users.permissions import IsModeratorPermission
 
 
 class CourseViewSet(ModelViewSet):
@@ -16,16 +18,30 @@ class CourseViewSet(ModelViewSet):
         serializer.save(creator=self.request.user)
 
     def get_permissions(self):
-        if self.action in ['create', 'destroy']:
-            self.permission_classes = (~IsModeratorPermission,)
-        self.permission_classes = (IsModeratorPermission,)
+        if self.action == "create":
+            self.permission_classes = (
+                IsAuthenticated,
+                ~IsModeratorPermission,
+            )
+        elif self.action == "destroy":
+            self.permission_classes = (
+                IsAuthenticated,
+                ~IsModeratorPermission | IsOwnerPermission,
+            )
+        self.permission_classes = (
+            IsAuthenticated,
+            IsModeratorPermission | IsOwnerPermission,
+        )
         return super().get_permissions()
 
 
 class LessonCreateApiView(CreateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = (~IsModeratorPermission,)
+    permission_classes = (
+        IsAuthenticated,
+        ~IsModeratorPermission,
+    )
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
@@ -34,22 +50,31 @@ class LessonCreateApiView(CreateAPIView):
 class LessonRetrieveApiView(RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = (IsModeratorPermission,)
+    permission_classes = (
+        IsAuthenticated,
+        IsModeratorPermission | IsOwnerPermission,
+    )
 
 
 class LessonListApiView(ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = (IsModeratorPermission,)
+    permission_classes = (IsAuthenticated, IsModeratorPermission)
 
 
 class LessonUpdateApiView(UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = (IsModeratorPermission,)
+    permission_classes = (
+        IsAuthenticated,
+        IsModeratorPermission | IsOwnerPermission,
+    )
 
 
 class LessonDestroyApiView(DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = (~IsModeratorPermission,)
+    permission_classes = (
+        IsAuthenticated,
+        ~IsModeratorPermission | IsOwnerPermission,
+    )
