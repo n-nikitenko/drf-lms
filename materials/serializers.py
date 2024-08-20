78139055661c189b64ce218798f69195adade7fe
Nina Nikitenko <ninanienliya@gmail.com>
2024-08-20 21:08:29 +0300

    реализована дополнительная проверка на отсутствие в материалах ссылок на сторонние ресурсы, кроме youtube.com.

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
+from materials.validators import validate_links
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(validators=[validate_links])
+    description = serializers.CharField(validators=[validate_links], required=False)
+    video_url = serializers.CharField(validators=[validate_links], required=False)
+
     class Meta:
         model = Lesson
         fields = "__all__"
@@ -13,6 +18,8 @@ class LessonSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    title = serializers.CharField(validators=[validate_links])
+    description = serializers.CharField(validators=[validate_links], required=False)
 
     @staticmethod
     def get_lessons_count(course):
